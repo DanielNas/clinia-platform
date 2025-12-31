b202fdf7d60545c03a345097fe150a2d6c48a1a6
Daniel Pereira do Nascimento <100876927+DanielNas@users.noreply.github.com>
2025-12-31 11:01:43 -0300

    Stage (migração com sucesso do prima e conexão com banco feita com sucesso)

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -4,16 +4,9 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "src/prisma/schema.prisma",
-
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL!,
-    },
-  },
-
-  migrations: {
-    path: "src/prisma/migrations",
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL,
   },
 });
 
