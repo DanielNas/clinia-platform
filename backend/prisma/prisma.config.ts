5309e4ca4c996aaffb742f6b96f2f8b82047027f
Daniel Pereira do Nascimento <100876927+DanielNas@users.noreply.github.com>
2026-01-02 15:44:18 -0300

    Revert "Stage (migração com sucesso do prima e conexão com banco feita com sucesso)"
    This reverts commit 8b3aecd9cfc6db930e955f939fc6bc7d1c91ab52.

--- backend/prisma/prisma.config.ts
+++ backend/prisma/prisma.config.ts
@@ -4,9 +4,16 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL,
+  schema: "src/prisma/schema.prisma",
+
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL!,
+    },
+  },
+
+  migrations: {
+    path: "src/prisma/migrations",
   },
 });
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,5 +4,5 @@ const PORT = 3000;
 
 // inicia o servidor
 app.listen(PORT, () => {
-    console.log(`Clinia APP rodando na porta ${PORT}`)
+    console.log('Clinia APP rodando na porta ${PORT}')
 });
